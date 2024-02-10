2960369680793f5ebfa0a6181dd7b9aa8bb62290
Eric <73071377+Posoroko@users.noreply.github.com>
2024-02-10 09:44:25 -0500

    seo links

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,9 +13,19 @@ export default defineNuxtConfig({
     app: {
         head: {
             link: [
-                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+                { rel: 'apple-touch-icon', sizes: '180x180', href: '/images/seo-assets/apple-touch-icon.png' },
+                { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/images/seo-assets/favicon-32x32.png' },
+                { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/images/seo-assets/favicon-16x16.png' },
+                { rel: 'manifest', href: '/images/seo-assets/site.webmanifest' },
+                { rel: 'mask-icon', href: '/images/seo-assets/safari-pinned-tab.svg', color: '#5bbad5' },
+                { rel: 'shortcut icon', href: '/images/seo-assets/favicon.ico' },
+                { rel: 'msapplication-TileColor', content: '#da532c' },
+                { rel: 'msapplication-config', content: '/images/seo-assets/browserconfig.xml' },
+                { rel: 'theme-color', content: '#ffffff' }
             ]    
         }
     }
 
 })
+
