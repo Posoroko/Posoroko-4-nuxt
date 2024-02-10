0b92594ee1d213d8a6c1926f60b00f59d5a5629c
Eric <73071377+Posoroko@users.noreply.github.com>
2024-02-10 10:02:31 -0500

    correcrtion

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,3 @@
-<script setup lang="ts">
-    useSeoMeta({
-        title: "Eric Podhorecki, développeur d'application web",
-        ogTitle: "Eric Podhorecki, développeur d'application web",
-        description: "Eric Podhorecki est un développeur web full-stack spécialisé dnas le développement de sites web et d'applications web.",
-        ogDescription: "Eric Podhorecki est un développeur web full-stack spécialisé dnas le développement de sites web et d'applications web.",
-        ogImage: "/ipmages/seo-assets/og-banner.jpg",
-        twitterCard: "summary_large_image",
-})
-</script>
-
 <template>
     <main class="grow">
 
