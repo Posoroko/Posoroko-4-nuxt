ee0b75fbc5f60c9c7e6827c12a14201ed45849fd
Eric <73071377+Posoroko@users.noreply.github.com>
2024-02-10 09:40:37 -0500

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,14 @@ export default defineNuxtConfig({
         name: "Eric Podhorecki, développeur web full-stack",
         description: "Eric Podhorecki est un développeur web full-stack spécialisé dnas le développement de sites web et d'applications web.",
         defaultLocale: 'fr', // not needed if you have @nuxtjs/i18n installed
+    },
+
+    app: {
+        head: {
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            ]    
+        }
     }
 
 })
